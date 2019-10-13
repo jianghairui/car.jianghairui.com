d3d3532c3d3352cda1e768d273e8f74bab03e869
JHR <1873645345@qq.com>
2019-10-13 22:51:07 +0800

    开发环境部署

--- application/admin/controller/Activity.php
+++ application/admin/controller/Activity.php
@@ -7,11 +7,181 @@
  */
 namespace app\admin\controller;
 
+use think\Db;
 class Activity extends Base {
 
     public function activityList() {
+        $param['search'] = input('param.search');
+        $page['query'] = http_build_query(input('param.'));
+        $curr_page = input('param.page',1);
+        $perpage = input('param.perpage',10);
+        $where = [];
+        if($param['search']) {
+            $where[] = ['title','like',"%{$param['search']}%"];
+        }
+        $count = Db::table('mp_activity')->alias('a')->where($where)->count();
+        $page['count'] = $count;
+        $page['curr'] = $curr_page;
+        $page['totalPage'] = ceil($count/$perpage);
+        try {
+            $list = Db::table('mp_activity')
+                ->where($where)
+                ->order(['id'=>'DESC'])
+                ->limit(($curr_page - 1)*$perpage,$perpage)->select();
+        }catch (\Exception $e) {
+            die('SQL错误: ' . $e->getMessage());
+        }
+        $this->assign('list',$list);
+        $this->assign('page',$page);
+        $this->assign('param',$param);
         return $this->fetch();
     }
 
+    public function activityAdd() {
+        return $this->fetch();
+    }
+
+    public function activityAddPost() {
+        $val['title'] = input('post.title');
+        $val['desc'] = input('post.desc');
+        checkInput($val);
+        $val['content'] = input('post.content');
+        $val['create_time'] = date('Y-m-d H:i:s');
+
+        if(isset($_FILES['file'])) {
+            $info = upload('file');
+            if($info['error'] === 0) {
+                $val['pic'] = $info['data'];
+            }else {
+                return ajax($info['msg'],-1);
+            }
+        }else {
+            return ajax('请上传封面图',-1);
+        }
+        try {
+            Db::table('mp_activity')->insert($val);
+        }catch (\Exception $e) {
+            if(isset($val['pic'])) {
+                @unlink($val['pic']);
+            }
+            return ajax($e->getMessage(),-1);
+        }
+        return ajax();
+    }
+
+    public function activityDetail() {
+        $val['id'] = input('param.id',0);
+        try {
+            $exist = Db::table('mp_activity')->where('id','=',$val['id'])->find();
+            if(!$exist) {
+                die('非法操作');
+            }
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+        $this->assign('info',$exist);
+        return $this->fetch();
+    }
+
+    public function activityMod() {
+        $val['title'] = input('post.title');
+        $val['desc'] = input('post.desc');
+        $val['id'] = input('post.id');
+        checkInput($val);
+        $val['content'] = input('post.content');
+        if(isset($_FILES['file'])) {
+            $info = upload('file');
+            if($info['error'] === 0) {
+                $val['pic'] = $info['data'];
+            }else {
+                return ajax($info['msg'],-1);
+            }
+        }
+        $where = [
+            ['id','=',$val['id']]
+        ];
+        try {
+            $exist = Db::table('mp_activity')->where($where)->find();
+            if(!$exist) {
+                return ajax('非法参数',-1);
+            }
+            Db::table('mp_activity')->where($where)->update($val);
+        }catch (\Exception $e) {
+            if(isset($val['pic'])) {
+                @unlink($val['pic']);
+            }
+            return ajax($e->getMessage(),-1);
+        }
+        if(isset($val['pic'])) {
+            @unlink($exist['pic']);
+        }
+        return ajax([]);
+    }
+
+    //停用活动
+    public function activityHide()
+    {
+        $val['id'] = input('post.id');
+        checkInput($val);
+        try {
+            $exist = Db::table('mp_activity')->where('id','=',$val['id'])->find();
+            if (!$exist) {
+                return ajax('非法操作', -1);
+            }
+            Db::table('mp_activity')->where('id','=',$val['id'])->update(['status' => 0]);
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        return ajax();
+    }
+    //启用活动
+    public function activityShow() {
+        $val['id'] = input('post.id');
+        checkInput($val);
+        try {
+            $exist = Db::table('mp_activity')->where('id','=',$val['id'])->find();
+            if(!$exist) {
+                return ajax('非法操作',-1);
+            }
+            Db::table('mp_activity')->where('id','=',$val['id'])->update(['status'=>1]);
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        return ajax();
+    }
+
+    //文章排序
+    public function sortActivity() {
+        $val['id'] = input('post.id');
+        $val['sort'] = input('post.sort');
+        checkInput($val);
+        try {
+            Db::table('mp_activity')->update($val);
+        }catch (\Exception $e) {
+            return ajax($e->getMessage(),-1);
+        }
+        return ajax($val);
+    }
+
+    public function activityDel() {
+        $val['id'] = input('post.id');
+        checkInput($val);
+        try {
+            $exist = Db::table('mp_activity')->where('id','=',$val['id'])->find();
+            if(!$exist) {
+                return ajax('非法操作',-1);
+            }
+            Db::table('mp_activity')->where('id','=',$val['id'])->delete();
+        }catch (\Exception $e) {
+            return ajax($e->getMessage(),-1);
+        }
+        @unlink($exist['pic']);
+        return ajax([],1);
+    }
+    
+
+    public function orderList() {
+        return $this->fetch();
+    }
 
 }
